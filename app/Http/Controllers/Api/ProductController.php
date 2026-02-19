84ac9b4aca1bf7c8b0f2b6bb7d4ca1e85961fc9d
wawanneutron <wawanneutron1331@gmail.com>
2026-02-19 13:11:19 +0700

    feat: product controller

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::latest()->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request)
+    {
+        $this->authorize('create', Product::class);
+
+        $product = Product::create($request->validated());
+
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $this->authorize('update', $product);
+
+        $product->update($request->validated());
+
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $this->authorize('delete', $product);
+
+        $product->delete();
+
+        return response()->json(['message' => 'Deleted']);
+    }
+}
